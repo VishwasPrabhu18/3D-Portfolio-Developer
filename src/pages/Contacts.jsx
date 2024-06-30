f12db15e0a14708aaec56c24b159f576ded19d32
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-06-30 10:33:33 +0530

    Contact page is completed

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,23 +1,32 @@
-import { useRef, useState } from "react";
+import { Suspense, useRef, useState } from "react";
 import emailjs from "@emailjs/browser"
+import { Canvas } from "@react-three/fiber";
+import Fox from "../models/Fox";
+import { Loader } from "@react-three/drei";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const { alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setForm({ ...form, [name]: value });
   };
 
-  const handleFocus = () => { };
+  const handleFocus = () => setCurrentAnimation("walk");
 
-  const handleBlur = () => { };
+  const handleBlur = () => setCurrentAnimation("idle");
 
   const handlesubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setCurrentAnimation("hit");
 
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -32,18 +41,27 @@ const Contact = () => {
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
     ).then(() => {
       setIsLoading(false);
-      setForm({ name: "", email: "", message: "" });
-      // TODO: Show success message
+      showAlert({ show: true, text: "Message sent successfully!", type: "success" });
+      
+      setTimeout(() => {
+        hideAlert();
+        setCurrentAnimation("idle");
+        setForm({ name: "", email: "", message: "" });
+      }, 3000);
 
     }).catch((error) => {
       setIsLoading(false);
       console.log(error);
+      setCurrentAnimation("idle");
+      showAlert({ show: true, text: "I didn't receive your message", type: "danger" });
       // TODO: Show error message
     });
   };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
@@ -71,6 +89,28 @@ const Contact = () => {
           >
             {isLoading ? "Sending..." : "Send Message"}</button>
         </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[50%] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}>
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
